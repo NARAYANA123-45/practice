be7f61188282d0d6abd0933e566c13d38714e701
NARAYANA123-45 <narayan.bujji@gmail.com>
2020-10-17 20:57:03 +0530

    skip

---
+++ script.js
@@ -0,0 +1,44 @@
+let Ul = document.getElementById('lists');
+  let Btn2= document.getElementById("btn");
+      Btn2.addEventListener("click",function Addelement(){
+  let Inputvalue= document.getElementById("input").value;
+  let Text=document.createTextNode(Inputvalue);
+     if(Inputvalue===""){
+      alert("Write something");
+     }
+    else{
+       let Li =document.createElement("li");
+      let Checkbox = document.createElement("input");
+        Checkbox.type = 'checkbox';
+     Checkbox.setAttribute('id','check');
+    let Label = document.createElement('label');
+    Ul.appendChild(Label);
+    Li.appendChild(Checkbox);
+	Li.appendChild(Label);	
+   Label.appendChild(Text);
+   Ul.appendChild(Li);
+ }
+  })
+                                             /*keyup event*/
+var Input2= document.getElementById("input");
+   Input2.addEventListener('keyup',function(e){
+    if (e.keyCode === 13) {
+   var Inputvalue= document.getElementById("input").value;
+   var Text=document.createTextNode(Inputvalue);
+     if(Inputvalue===""){
+      alert("Write something");
+     }
+	 else{
+       Li =document.createElement("li");
+      var Checkbox = document.createElement("input");
+        Checkbox.type = 'checkbox';
+     Checkbox.setAttribute('id','check');
+    var Label = document.createElement('label');
+    Ul.appendChild(Label);	
+    Li.appendChild(Checkbox);
+	Li.appendChild(Label);
+   Label.appendChild(Text);
+   Ul.appendChild(Li);
+ }
+  }
+	  })
